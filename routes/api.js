73230d7a42550482c840141e2f311bfbc2ae694d
xyqfer <xyqfer@gmail.com>
2017-12-10 13:37:36 +0800

    活动按时间排序

--- routes/api.js
+++ routes/api.js
@@ -219,6 +219,7 @@ router.get('/v1/activities', (req, res, next) => {
     let limit = req.query.limit || 5;
 
     query.limit(limit);
+    query.ascending('startTime');
     query.greaterThanOrEqualTo('endTime', new Date());
 
     query.find().then((results) => {
