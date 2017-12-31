2de1d0c591ebcd7ef058eb66c9a137f6ac3ba44c
xyqfer <xyqfer@gmail.com>
2017-12-31 16:19:33 +0800

    减少数据传输量

--- api/v1/uplabs/_loadData.js
+++ api/v1/uplabs/_loadData.js
@@ -14,12 +14,23 @@ module.exports = ({url, platform}) => {
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60'
         }
     }).then((data) => {
-        data = data.map((item) => {
+        return data.map((item) => {
+            let card = {
+                backgroundColor: item.background_color,
+                id: item.id,
+                animated: item.animated,
+                name: item.name,
+            };
+
+            card.desc = `by ${item.maker_name} in ${item.subcategory_friendly_name_plural}`;
+
             item.animated_teaser_url = item.animated_teaser_url.replace(assetReg, cdnHost) + '?imageView2/q/75';
 
             if (!item.animated && supportWebp) {
                 item.animated_teaser_url += '/format/webp';
             }
+
+            card.cover = item.animated_teaser_url;
 
             item.preview_url = item.preview_url.replace(assetReg, cdnHost);
 
@@ -27,21 +38,22 @@ module.exports = ({url, platform}) => {
                 item.preview_url += '?imageView2/format/webp';
             }
 
-            if (item.images.length > 0) {
-                item.images = item.images.map((img) => {
-                    img.urls.full = img.urls.full.replace(assetReg, cdnHost);
+            card.urls = [item.preview_url];
 
-                    if (!/\.gif$/g.test(img.urls.full) && supportWebp) {
-                        img.urls.full += '?imageView2/format/webp';
+            if (item.images.length > 0) {
+                item.images.forEach((img) => {
+                    let url = img.urls.full.replace(assetReg, cdnHost);
+
+                    if (!/\.gif$/g.test(url) && supportWebp) {
+                        url += '?imageView2/format/webp';
                     }
 
-                    return img;
+
+                    card.urls.push(url);
                 });
             }
 
-            return item;
+            return card;
         });
-
-        return data;
     });
 };
