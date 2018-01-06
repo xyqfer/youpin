ed18c9c1db6a804eedea6f2e5013df642436df71
xyqfer <xyqfer@gmail.com>
2018-01-06 18:07:11 +0800

    自动刷新页数改为3页

--- api/v1/uplabs/refreshOSS.js
+++ api/v1/uplabs/refreshOSS.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
     const loadData = require('./_loadData');
     const getFormatTime = require('./_formatTime');
 
-    const pageCount = 5;
+    const pageCount = 3;
     const timeObj = getFormatTime();
     const currentYear = timeObj.year;
     const currentMonth = timeObj.month;
