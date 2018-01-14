733f99723a5e88cb3a24c0c7e5db082209f1934f
xyqfer <xyqfer@gmail.com>
2018-01-14 23:36:01 +0800

    http 重定向至 https

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(AV.express());
 
 app.enable('trust proxy');
 // 需要重定向到 HTTPS
-// app.use(AV.Cloud.HttpsRedirect());
+app.use(AV.Cloud.HttpsRedirect());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
