7e5c0d58ee5549df2501e229f439fd7b34570f81
xyqfer <xyqfer@gmail.com>
2018-02-15 16:09:12 +0800

    Uplabs 添加 author 作品接口

--- api/v1/uplabs/author.js
+++ api/v1/uplabs/author.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = (req, res, next) => {
+    const loadData = require('./_loadData');
 
+    const params = req.params;
+    const author = params[0];
+    const page = req.query.page || 1;
+    const url = `https://www.uplabs.com/${author}.json?page=${page}`;
+
+    loadData({
+        url
+    }).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json([]);
+    });
 };
