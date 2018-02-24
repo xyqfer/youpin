e230c42fc71f6c27589f3df967a960352af10d3c
xyqfer <xyqfer@gmail.com>
2018-02-24 20:19:17 +0800

    添加 codepen 接口

--- api/v1/updateCodepen.js
+++ api/v1/updateCodepen.js
@@ -4,34 +4,42 @@ module.exports = (req, res) => {
     const AV = require('leanengine');
     const Promise = require('bluebird');
     const rp = require('request-promise');
-    const cheerio = require('cheerio');
+    const flatten = require('lodash/flatten');
+    const moment = require('moment');
     const sendMail = require('../lib/mail');
 
-    const dbName = 'Codrop';
+    const dbName = 'CodePen';
 
     function getCodepenData() {
-        return rp.get({
-            uri: 'https://codepen.io/pens/grid/picks/1/',
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-                'Accept-Language': 'en,zh-CN;q=0.9,zh;q=0.8,sk;q=0.7,ja;q=0.6,zh-TW;q=0.5',
-            }
-        }).then((data) => {
-            const result = JSON.parse(data);
-            const $ = cheerio.load(result.page.html);
-            const penList = [];
+        const pageCount = 3;
+        const pageCountList = [];
 
-            $('.single-pen').each(function () {
-                const $pen = $(this);
-                penList.push({
-                    url: $pen.find('.cover-link').attr('href'),
-                    title: $pen.find('.cover-link').text().replace(/\n+/g, '').trim()
+        for (let i = 1; i <= pageCount; i++) {
+            pageCountList.push(i);
+        }
+
+        return Promise.mapSeries(pageCountList, (page) => {
+            return rp.get({
+                json: true,
+                uri: `https://cpv2api.com/pens/picks?page=${page}`,
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+                    'Accept-Language': 'en,zh-CN;q=0.9,zh;q=0.8,sk;q=0.7,ja;q=0.6,zh-TW;q=0.5'
+                }
+            }).then((result) => {
+                return result.data.map((item) => {
+                    return {
+                        title: item.title,
+                        desc: item.details,
+                        url: item.link,
+                        cover: item.images.small
+                    };
                 });
+            }).catch((err) => {
+                console.log(err);
             });
-
-            res.json(penList);
-        }).catch((err) => {
-            console.log(err);
+        }).then((results) => {
+            return flatten(results);
         });
     }
 
@@ -43,43 +51,45 @@ module.exports = (req, res) => {
         return query.find();
     }
 
-    getCodepenData().then((result) => {
+    return Promise.all([getDbData(), getCodepenData()]).then((results) => {
+        const [ dbData, codepenData ] = results;
+
+        const newData = codepenData.filter((item) => {
+            for (let i = 0; i < dbData.length; i++) {
+                if (dbData[i].get('url') === item.url) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        if (newData.length > 0) {
+            let mailContent = '';
+
+            const CodePen = AV.Object.extend(dbName);
+            const codepenList = newData.map((item) => {
+                const codepen = new CodePen();
+                codepen.set('title', item.title);
+                codepen.set('desc', item.desc);
+                codepen.set('url', item.url);
+                codepen.set('cover', item.cover);
+
+                return codepen;
+            });
+
+            AV.Object.saveAll(codepenList).then((results) => {
+
+            }).catch((err) => {
+                console.log(err);
+            });
+
+
+
+            sendMail({
+                title: 'CodePen 有更新啦',
+                mailContent
+            });
+        }
     });
-
-
-    // return Promise.all([getDbData(), getCodropData()]).then((result) => {
-    //     const dbData = result[0];
-    //     const codropData = result[1];
-    //
-    //     const newData = codropData.filter((codropItem) => {
-    //         for (let i = 0; i < dbData.length; i++) {
-    //             if (dbData[i].get('postId') === codropItem.postId) {
-    //                 return false;
-    //             }
-    //         }
-    //
-    //         return true;
-    //     });
-    //
-    //     if (newData.length > 0) {
-    //         let mailContent = '';
-    //
-    //         newData.forEach((item) => {
-    //             mailContent += `<a href='${item.url}'>${item.name}</a><br><br>`;
-    //
-    //             const Codrop = AV.Object.extend(dbName);
-    //             const codrop = new Codrop();
-    //
-    //             codrop.set('postId', item.postId);
-    //             codrop.save(null, {
-    //                 useMasterKey: false
-    //             });
-    //         });
-    //
-    //         sendMail({
-    //             title: 'Codrop 更新啦',
-    //             mailContent: mailContent
-    //         });
-    //     }
-    // });
 };
