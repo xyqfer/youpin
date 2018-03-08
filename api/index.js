1a992e07b379a2d69a4d4a10919dc0c4f167ac86
xyqfer <xyqfer@gmail.com>
2018-03-08 16:06:40 +0800

    重写有品

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const router = require('express').Router();
-const apiV1 = require('./v1/index');
-const apiV2 = require('./v2/index');
+const apiV1 = require('./v1');
+const apiV2 = require('./v2');
 
 router.use('/v1', apiV1);
 router.use('/v2', apiV2);
---
+++ api/v1/updateYoupin/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = (req, res, next) => {
+    const getYoupinData = require('./getYoupinData');
+    const saveYoupinData = require('./saveYoupinData');
+    const {
+        db: {
+            getDbData
+        },
+        params,
+        mail: sendMail
+    } = require('app-lib');
+
+    const dbName = 'Mi_store';
+
+    (async () => {
+        const dbData = await getDbData({
+            dbName,
+            query: {
+                descending: ['updatedAt']
+            }
+        });
+
+        const youpinData = await getYoupinData();
+
+        const newData = youpinData.filter((item) => {
+            const gid = item.gid;
+
+            for (let i = 0; i < dbData.length; i++) {
+                if (gid == dbData[i].gid) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        if (newData.length > 0 && params.env !== 'development') {
+            saveYoupinData({
+                dbName,
+                data: newData
+            });
+            sendMail({
+                title: '米家上新品啦',
+                data: newData,
+                template: (url = '', name = '') => {
+                    return `<a href='${url}'>${name}</a><br><br>`;
+                }
+            });
+        }
+    })();
+
+    res.end();
+};
