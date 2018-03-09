40dd22a94b6d298e0f14c6cfcef3912a5e38e245
xyqfer <xyqfer@gmail.com>
2018-03-09 10:37:50 +0800

    updateDaily 重写完成

--- api/v1/updateDaily/index.js
+++ api/v1/updateDaily/index.js
@@ -2,16 +2,16 @@
 
 // 每日更新一次
 module.exports = (req, res) => {
-    const updateGitHubTrending = require('./updateGitHubTrending');
-    const updateCodrop = require('./updateCodrop');
-    const updateBook = require('./book/update');
+    const updateGitHubTrending = require('../updateGitHubTrending');
+    const updateCodrop = require('../updateCodrop');
+    const updateBook = require('../book/update');
 
     updateBook().then(() => {
         return updateCodrop();
     }).then(() => {
         return updateGitHubTrending();
     }).catch((err) => {
-        console.log(err);
+        console.error(err);
     });
 
     res.end();
