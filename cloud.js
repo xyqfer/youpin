112526c8f0ff58b2ff25641290d4c14a12c9be6f
xyqfer <xyqfer@gmail.com>
2018-03-19 09:59:48 +0800

    remove hackernews

--- cloud.js
+++ cloud.js
@@ -25,7 +25,7 @@ const cloudFuncConfig = [
         info: '更新 ZhihuHot'
     }, {
         name: 'update3PerDay',
-        module: ['updateSMZDM', 'updateJuejin', 'updateHackerNews'],
+        module: ['updateSMZDM', 'updateJuejin'],
         info: '每日三更'
     }, {
         name: 'updateDaily',
