8f688158cca65bc4288c39f2f528a79f98cf7c7f
xyqfer <xyqfer@gmail.com>
2018-03-21 14:51:02 +0800

    更新 smzdm

--- app-modules/updateSMZDM/getSMZDMData.js
+++ app-modules/updateSMZDM/getSMZDMData.js
@@ -1,8 +1,6 @@
 'use strict';
 
-module.exports = async ({
-    offsets = [1]
-}) => {
+module.exports = async () => {
     const Promise = require('bluebird');
     const rp = require('request-promise');
     const cheerio = require('cheerio');
@@ -12,7 +10,7 @@ module.exports = async ({
         params
     } = require('app-libs');
 
-    const results = await Promise.mapSeries(offsets, async (offset) => {
+    const results = await Promise.mapSeries([1, 2, 3], async (offset) => {
         try {
             const htmlString = await rp.get({
                 uri: `https://news.m.smzdm.com//ajax_get_list_html//${offset}`,
@@ -29,7 +27,7 @@ module.exports = async ({
 
                 newsList.push({
                     title: $elem.find('h2').text(),
-                    url: `http:${$elem.find('.openApp').attr('href')}`,
+                    url: `https:${$elem.find('.openApp').attr('href')}`,
                     cover: $elem.find('.image > img').attr('src'),
                     desc: $elem.find('p').text()
                 });
@@ -41,6 +39,49 @@ module.exports = async ({
             return [];
         }
     });
+
+    const postHtmlString = await rp.get({
+        uri: 'https://post.m.smzdm.com/',
+        headers: {
+            'User-Agent': params.ua.mobile
+        }
+    });
+
+    const $ = cheerio.load(postHtmlString);
+
+    $('#J_scroll_ul > .card-group-list').each(function () {
+        const $elem = $(this);
+
+        results.push({
+            title: $elem.find('.zm-card-title').text(),
+            url: `https:${$elem.find('a').attr('href')}`,
+            cover: $elem.find('.zm-card-media > img').attr('src'),
+            desc: ''
+        });
+    });
+
+    let timesort = $('#J_scroll_ul > .card-group-list').last().attr('timesort');
+
+    for (let i = 1; i <= 5; i++) {
+        const postData = await rp.get({
+            json: true,
+            uri: `https://post.m.smzdm.com/ajax_get_list_html/?timesort=${timesort}`,
+            headers: {
+                'User-Agent': params.ua.mobile
+            }
+        });
+
+        postData.data.forEach((item) => {
+            results.push({
+                title: item.article_title,
+                url: `https:${item.article_url}`,
+                cover: item.article_pic,
+                desc: item.article_region_title
+            });
+        });
+
+        timesort = postData.data[postData.data.length - 1].time_sort;
+    }
 
     return uniqBy(flatten(results), 'url');
 };
