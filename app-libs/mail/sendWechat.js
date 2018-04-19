7f20413025598002acd268dbab211bc82e75eb0c
xyqfer <xyqfer@gmail.com>
2018-04-19 11:05:42 +0800

    markdown 不显示 emoji

--- app-libs/mail/sendWechat.js
+++ app-libs/mail/sendWechat.js
@@ -8,12 +8,14 @@ module.exports = async ({ title = '', content = '' }) => {
     let markdownContent = '';
 
     const cheerioId = 'J-cheerio-wrap';
+    const emojiReg = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig;
     const $ = cheerio.load(`<div id='${cheerioId}'>${content}</div>`);
+
     $(`#${cheerioId}`).children().each(function () {
         const $elem = $(this);
 
         const url = $elem.find('a').attr('href');
-        const title = $elem.find('a').text();
+        const title = $elem.find('a').text().replace(emojiReg, '');
 
         markdownContent += `- [${title}](${url})
 `;
