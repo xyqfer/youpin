8f9cb369f5c94af9ce14d99bd80a53a10f7e16ad
xyqfer <xyqfer@gmail.com>
2018-07-22 11:21:26 +0800

    添加 pwa

---
+++ public/v2ex/m/precache-manifest.05790e83cbf2cbee04cffe81ee8c7326.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4348368ab857cb57b18e1a87ba8ff284",
+    "url": "/v2ex/m/fonts/Framework7Icons-Regular.4348368a.ttf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/v2ex/m/robots.txt"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/v2ex/m/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "cd688f0ca6d1e94ac74a",
+    "url": "/v2ex/m/js/chunk-vendors.cd688f0c.js"
+  },
+  {
+    "revision": "8bfeae6b4de6ca025ad2",
+    "url": "/v2ex/m/js/app.8bfeae6b.js"
+  },
+  {
+    "revision": "7a654aec2d02ce0d666baf675c6f854a",
+    "url": "/v2ex/m/index.html"
+  },
+  {
+    "revision": "3cfdd12c6423634b723590811a9eed9c",
+    "url": "/v2ex/m/img/f7-icon.3cfdd12c.png"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/v2ex/m/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/v2ex/m/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/v2ex/m/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "c0087d4d5ddd32ec52f1859b42956db6",
+    "url": "/v2ex/m/fonts/Framework7Icons-Regular.c0087d4d.eot"
+  },
+  {
+    "revision": "ae8767ca8ecf4b090af58eff5dd6d3a7",
+    "url": "/v2ex/m/fonts/Framework7Icons-Regular.ae8767ca.woff2"
+  },
+  {
+    "revision": "2e9a0313ed02aed1e206340feb36ec5c",
+    "url": "/v2ex/m/fonts/Framework7Icons-Regular.2e9a0313.woff"
+  },
+  {
+    "revision": "8bfeae6b4de6ca025ad2",
+    "url": "/v2ex/m/css/app.333f9dd3.css"
+  }
+];
