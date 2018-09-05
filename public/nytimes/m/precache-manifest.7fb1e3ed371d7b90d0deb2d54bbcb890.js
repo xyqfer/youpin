69b6a7fc39185957f5b74de81863a82704698ebe
xyqfer <xyqfer@gmail.com>
2018-09-05 16:38:17 +0800

    update

---
+++ public/nytimes/m/precache-manifest.7fb1e3ed371d7b90d0deb2d54bbcb890.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0087d4d5ddd32ec52f1859b42956db6",
+    "url": "/nytimes/m/fonts/Framework7Icons-Regular.c0087d4d.eot"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nytimes/m/robots.txt"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/nytimes/m/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "61975181360025ff8d70",
+    "url": "/nytimes/m/css/app.c6e8a214.css"
+  },
+  {
+    "revision": "7bb862c7fa673db61991",
+    "url": "/nytimes/m/js/2.7bb862c7.js"
+  },
+  {
+    "revision": "c06cff9bf779ff7f6e96",
+    "url": "/nytimes/m/js/chunk-vendors.c06cff9b.js"
+  },
+  {
+    "revision": "ae8767ca8ecf4b090af58eff5dd6d3a7",
+    "url": "/nytimes/m/fonts/Framework7Icons-Regular.ae8767ca.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/nytimes/m/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "61975181360025ff8d70",
+    "url": "/nytimes/m/js/app.61975181.js"
+  },
+  {
+    "revision": "8ed1b1565b0f6b03fafb",
+    "url": "/nytimes/m/js/1.8ed1b156.js"
+  },
+  {
+    "revision": "4348368ab857cb57b18e1a87ba8ff284",
+    "url": "/nytimes/m/fonts/Framework7Icons-Regular.4348368a.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/nytimes/m/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "09cb9966e6ff69cca6694ec5d1f11c86",
+    "url": "/nytimes/m/img/background_1.09cb9966.jpg"
+  },
+  {
+    "revision": "2e9a0313ed02aed1e206340feb36ec5c",
+    "url": "/nytimes/m/fonts/Framework7Icons-Regular.2e9a0313.woff"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/nytimes/m/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "008addc9259906bb911fe1317193ea02",
+    "url": "/nytimes/m/index.html"
+  },
+  {
+    "revision": "8ed1b1565b0f6b03fafb",
+    "url": "/nytimes/m/css/1.c01f40c7.css"
+  }
+];
