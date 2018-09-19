ec95871507abcd09a522b438fca9cd65ce4bf4cb
xyqfer <xyqfer@gmail.com>
2018-09-19 15:23:31 +0800

    翻译接口拆分

---
+++ api/v1/nytimes/utils/googletranslate.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * google 翻译
+ */
+
+module.exports = (text = '') => {
+  const Promise = require('bluebird');
+  const rp = require('request-promise');
+  const { params } = require('app-libs');
+
+  const form = {
+    dt: 't',
+    tl: 'zh-CN',
+    ie: 'UTF-8',
+    sl: 'auto',
+    client: 'ia',
+    dj: '1',
+    q: text,
+  };
+
+  return Promise.reduce(['cn', 'com'], function(total, current) {
+    if (!total) {
+      return rp.post({
+        json: true,
+        uri: `https://translate.google.${current}/translate_a/single`,
+        form,
+        headers: {
+          'User-Agent': params.ua.mobile,
+        },
+      }).then((resp) => {
+        return resp;
+      });
+    } else {
+      return total;
+    }
+  }, null);
+};
