c36ce96b71baf8c1181309b3d59a5a7406c512e2
xyqfer <xyqfer@gmail.com>
2018-10-08 14:48:41 +0800

    添加 yande.re

--- app-modules/updateKonachan/getKonachanData.js
+++ app-modules/updateKonachan/getKonachanData.js
@@ -1,20 +1,35 @@
 'use strict';
 
 module.exports = async () => {
+    const Promise = require('bluebird');
     const rp = require('request-promise');
     const {
         params
     } = require('app-libs');
 
-    const result = await rp.get({
-        json: true,
-        uri: 'https://rsshub.app/konachan/post/popular_recent.json',
-        headers: {
-            'User-Agent': params.ua.pc,
+    const urls = [
+        'https://rsshub.app/konachan/post/popular_recent.json',
+        'https://rsshub.app/yande.re/post/popular_recent.json'
+    ];
+
+    const data = await Promise.mapSeries(urls, async (url) => {
+        try {
+            const result = await rp.get({
+                json: true,
+                uri: url,
+                headers: {
+                    'User-Agent': params.ua.pc
+                },
+            });
+
+            return result.items;
+        } catch (err) {
+            console.error(err);
+            return [];
         }
     });
 
-    return result.items.map(({ title, summary, url }) => {
+    return flatten(data).map(({ title, summary, url }) => {
         return {
             title,
             url,
