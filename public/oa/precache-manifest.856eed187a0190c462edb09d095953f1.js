b67007e33853c9a4f19df6fd1bc2f38df75c4926
xyqfer <xyqfer@gmail.com>
2018-10-12 15:21:56 +0800

    deploy oa pages

---
+++ public/oa/precache-manifest.856eed187a0190c462edb09d095953f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/oa/robots.txt"
+  },
+  {
+    "revision": "dc468a11dbe9c7bd5065ac323b101690",
+    "url": "/oa/js/xlsx.full.min.js"
+  },
+  {
+    "revision": "bba1f47c1ffee2cb6cd3",
+    "url": "/oa/js/chunk-vendors.a9221d78.js"
+  },
+  {
+    "revision": "f07427a6d1c64a1c8e97",
+    "url": "/oa/js/app.79638882.js"
+  },
+  {
+    "revision": "2c2e442afaa6c1f39d79a74e2c319c73",
+    "url": "/oa/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/oa/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/oa/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "bba1f47c1ffee2cb6cd3",
+    "url": "/oa/css/chunk-vendors.f31a5d75.css"
+  },
+  {
+    "revision": "f07427a6d1c64a1c8e97",
+    "url": "/oa/css/app.74a716f9.css"
+  }
+];
