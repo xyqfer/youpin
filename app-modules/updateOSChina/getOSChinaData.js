21f9deacd582f802c64d87c60a298d03d55a57c7
xyqfer <xyqfer@gmail.com>
2018-10-15 11:25:38 +0800

    添加 OSChina 乱弹

---
+++ app-modules/updateOSChina/getOSChinaData.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = async () => {
+    const rp = require('request-promise');
+    const cheerio = require('cheerio');
+    const {
+        params
+    } = require('app-libs');
+
+    const htmlString = await rp.get({
+        uri: 'https://my.oschina.net/xxiaobian/widgets/_space_index_newest_blog?catalogId=547834&q=&p=1&type=ajax',
+        headers: {
+            'User-Agent': params.ua.pc
+        }
+    });
+
+    const messages = [];
+    const $ = cheerio.load(htmlString);
+
+    $('.blog-item').each(function () {
+        const $item = $(this);
+        const $link = $item.find('.header');
+        const title = $link.text();
+
+        messages.push({
+            title,
+            url: $link.attr('href')
+        });
+    });
+
+    return messages;
+};
---
+++ app-modules/updateOSChina/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = async () => {
+    const updateContainer = require('app-containers/update');
+    const getOSChinaData = require('./getOSChinaData');
+
+    const filterKey = 'url';
+    const dbName = 'OSChina';
+
+    try {
+        return await updateContainer({
+            dbName,
+            filterKey,
+            mail: {
+                title: 'OSChina-每日乱弹有更新',
+                template: ({ title = '', url = '' }) => {
+                    return `
+                        <div style="margin-bottom: 50px">
+                            <h4>
+                              <a href="${url}">
+                                ${title}
+                              </a>
+                            </h4>
+                        </div>
+                    `;
+                }
+            },
+            getTargetData: () => {
+                return getOSChinaData();
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+};
