e8f8a64d91598c05164e225ca9af39356a7a2776
xyqfer <xyqfer@gmail.com>
2018-10-17 14:38:20 +0800

    添加知乎想法热榜

---
+++ app-modules/updateZhihuPinHot/getZhihuPinHotData.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = async () => {
+    const rp = require('request-promise');
+    const cheerio = require('cheerio');
+    const {
+        params
+    } = require('app-libs');
+
+    const xmlString = await rp.get({
+        uri: 'https://rsshub.app/zhihu/pin/hotlist',
+        headers: {
+            'User-Agent': params.ua.pc,
+        }
+    });
+
+    const $ = cheerio.load(xmlString, {
+        normalizeWhitespace: true,
+        xmlMode: true
+    });
+    const result = [];
+    
+    $('item').each(function() {
+        const $item = $(this);
+
+        result.push({
+            url: $item.find('link').text(),
+            title: $item.find('title').text(),
+            summary: $item.find('description').text()
+        });
+    });
+
+    return result;
+};
---
+++ app-modules/updateZhihuPinHot/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = async () => {
+    const updateContainer = require('app-containers/update');
+    const getZhihuPinHotData = require('./getZhihuPinHotData');
+
+    const filterKey = 'url';
+    const dbName = 'Dribbble';
+
+    try {
+        return await updateContainer({
+            dbName,
+            filterKey,
+            mail: {
+                title: '知乎想法热榜有更新~',
+                template: ({ title = '', summary = '', url = '' }) => {
+                    return `
+                        <div style="margin-bottom: 60px">
+                            <a href="${url}">
+                                <h4>${title}</h4>
+                            </a>
+                            <div>
+                                ${summary}
+                            </div>
+                        </div>
+                        <br><br>
+                    `;
+                }
+            },
+            getTargetData: () => {
+                return getZhihuPinHotData();
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+};
