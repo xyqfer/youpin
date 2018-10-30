2dea89236a2f8c7ef0c76b846ecc3efd8a809bd8
xyqfer <xyqfer@gmail.com>
2018-10-30 16:52:37 +0800

    update wake

--- app-modules/wake/index.js
+++ app-modules/wake/index.js
@@ -4,7 +4,7 @@ module.exports = () => {
     const rp = require('request-promise');
 
     rp.get({
-        uri: 'https://stg-sy2bnjwp1a.leanapp.cn'
+        uri: 'https://sy2bnjwp1a.leanapp.cn'
     }).catch(() => {
 
     });
