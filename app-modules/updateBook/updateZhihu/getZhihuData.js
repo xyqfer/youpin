96bb1687739e2a2b7952f17f5a4f39e351717b47
xyqfer <xyqfer@gmail.com>
2018-11-13 15:20:48 +0800

    add zhihu book

---
+++ app-modules/updateBook/updateZhihu/getZhihuData.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = async () => {
+    const { params, http } = require('app-libs');
+
+    try {
+        const result = await http.get({
+            uri: 'https://api.zhihu.com/books/features/new',
+            json: true,
+            headers: {
+                'User-Agent': params.ua.pc
+            }
+        });
+
+        return result.data.map(book => {
+            const authors = book.authors.map((author) => author.name).join('、');
+
+            return {
+                name: book.title,
+                url: book.url,
+                cover: book.cover.replace(/_.+\.jpg/g, '.jpg'),
+                pubInfo: authors,
+                desc: book.description
+            };
+        });
+    } catch (err) {
+        console.error(err);
+        return [];
+    }
+};
---
+++ app-modules/updateBook/updateZhihu/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = async () => {
+    const updateContainer = require('app-containers/update');
+    const getZhihuData = require('./getZhihuData');
+
+    try {
+        return await updateContainer({
+            dbName: 'DoubanBook',
+            mail: {
+                title: '知乎有新书啦~',
+                template: ({ name = '', url = '', pubInfo = '', desc = '', cover = '' }) => {
+                    return `
+                        <div style="margin-bottom: 60px">
+                            <a href="${url}">
+                                <h4>${name}</h4>
+                            </a>
+                            <p>
+                                ${pubInfo}
+                            </p>
+                            <div>
+                                ${desc}
+                            </div>
+                            <div>
+                                <img src="${cover}" 
+                                    alt="">
+                            </div>
+                        </div>
+                        <br><br>
+                    `;
+                }
+            },
+            getTargetData: () => {
+                return getZhihuData();
+            },
+            filterKey: 'url'
+        });
+    } catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+};
