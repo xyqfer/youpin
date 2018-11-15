224188d9fe8ea23182ff0f2e6b3464964e43080d
xyqfer <xyqfer@gmail.com>
2018-11-15 10:18:21 +0800

    add new rss

--- app-modules/updateJuejinPinHot/getPinData.js
+++ app-modules/updateJuejinPinHot/getPinData.js
@@ -1,33 +1,50 @@
 'use strict';
 
 module.exports = async () => {
+    const Promise = require('bluebird');
     const cheerio = require('cheerio');
+    const flatten = require('lodash/flatten');
     const {
         params,
-        http
+        http,
     } = require('app-libs');
 
-    const xmlString = await http.get({
-        uri: 'https://rsshub.app/juejin/pins',
-        headers: {
-            'User-Agent': params.ua.pc,
+    const urls = [
+        'https://rsshub.app/juejin/pins',
+        'https://rsshub.avosapps.us/weseepro/newest',
+        'https://rsshub.avosapps.us/weseepro/circle',
+    ];
+
+    const data = await Promise.mapSeries(urls, async (url) => {
+        try {
+            const xmlString = await http.get({
+                uri: url,
+                headers: {
+                    'User-Agent': params.ua.pc
+                },
+            });
+            const $ = cheerio.load(xmlString, {
+                normalizeWhitespace: true,
+                xmlMode: true
+            });
+            const result = [];
+
+            $('item').each(function () {
+                const $item = $(this);
+
+                result.push({
+                    url: $item.find('link').text(),
+                    title: $item.find('title').text() || '',
+                    summary: $item.find('description').text()
+                });
+            });
+
+            return result;
+        } catch (err) {
+            console.error(err);
+            return [];
         }
     });
 
-    const $ = cheerio.load(xmlString, {
-        normalizeWhitespace: true,
-        xmlMode: true
-    });
-    const result = [];
-    $('item').each(function() {
-        const $item = $(this);
-
-        result.push({
-            url: $item.find('link').text(),
-            title: $item.find('title').text(),
-            summary: $item.find('description').text()
-        });
-    });
-
-    return result;
+    return flatten(data);
 };
