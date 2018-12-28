0c418e9f46f36606bfe3f3fa6a6e9ab96ca23494
xyqfer <xyqfer@gmail.com>
2018-12-28 16:04:23 +0800

    update douban book

--- app-modules/updateBook/updateDouban/getDoubanData.js
+++ app-modules/updateBook/updateDouban/getDoubanData.js
@@ -1,64 +1,26 @@
 'use strict';
 
 module.exports = async () => {
-    const rp = require('request-promise');
-    const cheerio = require('cheerio');
+    const Parser = require('rss-parser');
     const uniqBy = require('lodash/uniqBy');
-    const { params } = require('app-libs');
-    const bookList = [];
+    let parser = new Parser();
 
-    try {
-        // 新书页
-        let htmlString = await rp.get({
-            uri: 'https://book.douban.com/latest?icn=index-latestbook-all',
-            headers: {
-                'User-Agent': params.ua.pc
-            }
-        });
-        let $ = cheerio.load(htmlString);
+    const feed = await parser.parseURL('https://rsshub.avosapps.us/douban/book/latest');
+    const bookList = feed.items.map((item) => {
+        const url = item.link;
+        const cover = '';
+        const name = item.title;
+        const desc = item.content;
+        const pubInfo = '';
 
-        $('#content ul > li').each(function () {
-            const $book = $(this);
-
-            bookList.push({
-                url: $book.find('.cover').attr('href'),
-                cover: $book.find('.cover > img').attr('src'),
-                name: $book.find('.detail-frame > h2').text().replace(/\n+/g, '').trim(),
-                desc: $book.find('.detail').text().replace(/\n+/g, '').trim(),
-                pubInfo: $book.find('.color-gray').text().replace(/\n+/g, '').trim()
-            });
-        });
-    } catch (err) {
-        console.error(err);
-    }
-
-    try {
-        //读书首页
-        const htmlString = await rp.get({
-            uri: 'https://book.douban.com/',
-            headers: {
-                'User-Agent': params.ua.pc
-            }
-        });
-        const $ = cheerio.load(htmlString);
-
-        $('.carousel .slide-list li').each(function () {
-            const $book = $(this);
-            const pubInfo = $book.find('.more-meta .author').text().replace(/\n+/g, '').trim() + ' / ' +
-                $book.find('.more-meta .publisher').text().replace(/\n+/g, '').trim() + ' / ' +
-                $book.find('.more-meta .year').text().replace(/\n+/g, '').trim();
-
-            bookList.push({
-                url: $book.find('.cover a').attr('href').replace(/\?.+$/g, ''),
-                cover: $book.find('.cover img').attr('src'),
-                name: $book.find('.info .more-meta .title').text().replace(/\n+/g, '').trim(),
-                desc: $book.find('.abstract').text().replace(/\n+/g, '').trim(),
-                pubInfo
-            });
-        });
-    } catch (err) {
-        console.error(err);
-    }
+        return {
+            url,
+            cover,
+            name,
+            desc,
+            pubInfo,
+        };
+    });
 
     return uniqBy(bookList, 'url');
 };
