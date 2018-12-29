c4c04ac09a46efb254e99a7f4a5a57be7cfbb038
xyqfer <xyqfer@gmail.com>
2018-12-29 09:22:01 +0800

    hotfix noreferrer

--- app-libs/mail/index.js
+++ app-libs/mail/index.js
@@ -18,6 +18,7 @@ module.exports = async ({ title = '', data = [], render = 'archive', template = 
     const $ = cheerio.load(content);
     $('a').each(function() {
         $(this).attr('target', '_blank');
+        $(this).attr('rel', 'noreferrer');
     });
     content = $.html();
 
