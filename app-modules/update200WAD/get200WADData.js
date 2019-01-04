7eff72d49edbefb601c6a075284c1dbdd4a2626f
xyqfer <xyqfer@gmail.com>
2019-01-04 14:16:00 +0800

    update

---
+++ app-modules/update200WAD/get200WADData.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = async () => {
+    const Promise = require('bluebird');
+    const cheerio = require('cheerio');
+    const rp = require('request-promise');
+    const flatten = require('lodash/flatten');
+    const {
+        params
+    } = require('app-libs');
+
+    const urls = [
+        'https://rsshub.avosapps.us/200wordsaday/hot?limit=3',
+    ];
+
+    const data = await Promise.mapSeries(urls, async (url) => {
+        try {
+            const xmlString = await rp.get({
+                uri: url,
+                headers: {
+                    'User-Agent': params.ua.pc
+                },
+            });
+            const $ = cheerio.load(xmlString, {
+                normalizeWhitespace: true,
+                xmlMode: true
+            });
+            const result = [];
+
+            $('item').each(function() {
+                const $item = $(this);
+
+                result.push({
+                    url: $item.find('link').text(),
+                    title: $item.find('title').text() || '',
+                    summary: $item.find('description').text()
+                });
+            });
+
+            return result;
+        } catch (err) {
+            console.error(err);
+            return [];
+        }
+    });
+
+    return flatten(data);
+};
