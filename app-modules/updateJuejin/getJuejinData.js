6fd2613d5bd11f8596bfbfab85280013cb6dfc89
xyqfer <xyqfer@gmail.com>
2019-02-04 10:45:13 +0800

    update

--- app-modules/updateJuejin/getJuejinData.js
+++ app-modules/updateJuejin/getJuejinData.js
@@ -13,32 +13,27 @@ module.exports = async ({
 
     const results = await Promise.mapSeries(offsets, async (offset) => {
         try {
-            const result = await rp.post({
+            const result = await rp.get({
                 json: true,
-                uri: 'http://api.xitu.io/resources/gold',
+                uri: 'https://timeline-merger-ms.juejin.im/v1/get_tag_entry?src=web&tagId=5597a05ae4b08a686ce56f6f&page=0&pageSize=50&sort=createdAt',
                 headers: {
                     'User-Agent': params.ua.pc,
                 },
-                body: {
-                    category: 'frontend',
-                    order: 'time',
-                    offset: offset,
-                    limit: 30
-                }
             });
 
-            return result.data;
+            return result.d.entrylist;
         } catch (err) {
             console.error(err);
             return [];
         }
     });
 
-    return uniqBy(flatten(results), 'id').map((item) => {
+    return uniqBy(flatten(results), 'objectId').map((item) => {
         return {
-            postId: item.id,
+            postId: item.objectId,
             title: item.title,
-            url: item.originalUrl
+            url: item.originalUrl,
+            summary: item.summaryInfo,
         };
     });
 };
