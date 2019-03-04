aa4481f6a99cb2858d566b6409c9f7cfcc4cbf42
xyqfer <xyqfer@gmail.com>
2019-03-04 15:26:14 +0800

    update

--- app-modules/updateDDK/getData.js
+++ app-modules/updateDDK/getData.js
@@ -85,11 +85,71 @@ module.exports = async () => {
             };
         });
     };
+    const getKSContent = async () => {
+        const result = await http.get({
+            json: true,
+            uri: 'https://www.chufanggushi.com/api/recipes/?page_size=1&page=1&type=recipe',
+            headers: {
+                'User-Agent': params.ua.pc,
+                'Accept-Language': 'zh',
+            },
+        });
+
+        return result.data.map((item) => {
+            let summary = '';
+
+            if (item.video) {
+                const video = item.video;
+                summary = `
+                    <video src="${video.video_url}" controls="controls" poster="${video.preview_image.url}" style="width: 100%"></video>
+                `;
+            } else {
+                summary = `
+                    <img src="${item.detail_image.url}" referrerpolicy="no-referrer">
+                `;
+            }
+
+            summary += '<br>';
+            summary += '<div><strong>食材:</strong></div>';
+            summary += item.ingredients.reduce((acc, item) => {
+                acc += `
+                    <div>${item.name}${item.amount}${item.unit}</div>
+                `;
+                return acc;
+            }, '');
+
+            summary += '<br>';
+            summary += '<div><strong>餐具:</strong></div>';
+            summary += item.utensils.reduce((acc, item) => {
+                acc += `
+                    <div>${item.name}</div>
+                `;
+                return acc;
+            }, '');
+
+            summary += '<br>';
+            summary += item.steps.reduce((acc, item, index, steps) => {
+                acc += `
+                    <div><strong>步骤 ${index + 1}/${steps.length}</strong></div>
+                    <div><img src="${item.image.url}" referrerpolicy="no-referrer"></div>
+                    <div>${item.text}</div>
+                `;
+                return acc;
+            }, '');
+
+            return {
+                url: item.url,
+                title: item.title,
+                summary: summary,
+            };
+        });
+    };
 
     try {
         const menuContent = await getMenuContent();
         const tvContent = await getTVContent();
-        return [...menuContent, ...tvContent];
+        const ksContent = await getKSContent();
+        return [...menuContent, ...tvContent, ...ksContent];
     } catch (err) {
         console.error(err);
         return [];
