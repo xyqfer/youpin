177af5ad2ceea43fce9b4ca94e1408e3789be15b
xyqfer <xyqfer@gmail.com>
2019-03-07 10:43:09 +0800

    add updateAlipayScheme

---
+++ app-modules/updateAlipayScheme/ipaExtract.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const plist = require('simple-plist');
+const decompress = require('decompress-zip');
+const rimraf = require('rimraf');
+const glob = require("glob");
+
+module.exports = function (file, callback) {
+  const outputPath = '/tmp/ipaExtract/';
+  let data = {};
+
+  var unzipper = new decompress(file);
+  unzipper.extract({
+    path: outputPath
+  });
+
+  unzipper.on('error', cleanUp);
+  unzipper.on('extract', function () {
+    const path = glob.sync(output.path + '/Payload/*/')[0];
+    data.metadata = plist.readFileSync(path + 'OpenPlatformAdapter.bundle/ACSettings.plist');
+    cleanUp();
+  });
+
+  function cleanUp(error) {
+    rimraf.sync(outputPath);
+    return callback(error, data);
+  }
+};
