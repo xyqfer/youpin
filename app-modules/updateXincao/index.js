04ec47b878f35689446cb82e3ddf2b6533703265
xyqfer <xyqfer@gmail.com>
2019-03-07 21:40:31 +0800

    添加燃气费

--- app-modules/updateXincao/index.js
+++ app-modules/updateXincao/index.js
@@ -7,22 +7,30 @@ module.exports = async () => {
 
   try {
     const title = '刷书啦~';
+    let data = [{
+      url: 'life1389://topic?topic_id=6612888851461439758&tab=1',
+      title,
+    }, {
+      title: '领蚂蚁积分啦~',
+      url: 'alipays://platformapi/startapp?appId=20000160',
+    }, {
+      title: '签到~',
+      url: 'mnw://',
+    }, {
+      title: '一个',
+      url: 'http://m.wufazhuce.com/'
+    }];
+    const now = new Date();
+    if (now.getDate() === 26) {
+      data.push({
+        title: '燃气费',
+        url: 'alipays://platformapi/startapp?appId=20000193'
+      });
+    }
 
     sendMail({
       title,
-      data: [{
-        url: 'life1389://topic?topic_id=6612888851461439758&tab=1',
-        title,
-      }, {
-        title: '领蚂蚁积分啦~',
-        url: 'alipays://platformapi/startapp?appId=20000160',
-      }, {
-          title: '签到~',
-          url: 'mnw://',
-      }, {
-        title: '一个',
-        url: 'http://m.wufazhuce.com/'
-      }],
+      data,
       template: ({ url = '', title = '' }) => {
         return `
             <div style="margin-bottom: 50px">
