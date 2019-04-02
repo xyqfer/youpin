a21410c8e1341ed1a058b76ac8496f52c9d5b071
xyqfer <xyqfer@gmail.com>
2019-04-02 16:58:35 +0800

    update

--- app-modules/updateTerminus/getTerminusData.js
+++ app-modules/updateTerminus/getTerminusData.js
@@ -2,14 +2,37 @@
 
 module.exports = async () => {
     const Parser = require('rss-parser');
-    let parser = new Parser();
+    const Promise = require('bluebird');
+    const flatten = require('lodash/flatten');
+    const parser = new Parser();
 
-    const feed = await parser.parseURL('https://terminus2049.github.io/atom.xml');
-    return feed.items.slice(0, 5).map((item) => {
+    const urls = [
+        'https://terminus2049.github.io/atom.xml',
+        'https://manjusaka.itscoder.com/atom.xml',
+    ];
+
+    const data = await Promise.mapSeries(urls, async (url) => {
+        try {
+            const feed = await parser.parseURL(url);
+
+            return feed.items.map(item => {
+                return {
+                    title: item.title,
+                    url: item.link,
+                };
+            });
+        } catch (err) {
+            console.error(err);
+            console.error(url);
+            return [];
+        }
+    });
+
+    return flatten(data).map(({ title, url }) => {
         return {
-            url: item.link,
-            title: item.title,
-            summary: ''
+            title,
+            url,
+            summary: '',
         };
     });
 };
