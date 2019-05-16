69c109686a48360cf660748bd5b880736b59f3ce
凉凉 <xyqfer@gmail.com>
2019-05-16 09:50:13 +0800

    update

--- app-modules/updateInitium/index.js
+++ app-modules/updateInitium/index.js
@@ -22,6 +22,7 @@ module.exports = async () => {
                         </div>
                     `;
                 },
+                device: 'device2',
             },
             getTargetData: () => {
                 return getData();
