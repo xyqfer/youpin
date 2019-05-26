76045a295be115bb0591b98f8d9f8b3257e685fa
凉凉 <xyqfer@gmail.com>
2019-05-26 17:52:37 +0800

    update

--- app-modules/updateBlog/updateTechBlog/getBlogData.js
+++ app-modules/updateBlog/updateTechBlog/getBlogData.js
@@ -62,6 +62,7 @@ module.exports = async () => {
     'https://rsshub.avosapps.us/webkit/blog?limit=3',
     'https://rsshub.avosapps.us/mozilla/blog?limit=3',
     'https://rsshub.avosapps.us/mozilla/hacks?limit=3',
+    'https://rsshub.avosapps.us/custom/https%3A%2F%2Fmozillagfx.wordpress.com%2Ffeed%2F?limit=1',
     'https://rsshub.avosapps.us/wechat/miniprogram/developers?limit=3',
     'https://rsshub.avosapps.us/custom/https%3A%2F%2Fwww.runningcheese.com%2Ffeed?limit=3',
     // 'https://rsshub.avosapps.us/medium/user/netflix-techblog?limit=3',
