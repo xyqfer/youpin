e8cd39e0c8d6ce5306b08eb9741c28967b5dce62
凉凉 <xyqfer@gmail.com>
2019-08-09 16:13:24 +0800

    update

--- app-modules/updateBook/updateDangdang/getDangdangData.js
+++ app-modules/updateBook/updateDangdang/getDangdangData.js
@@ -5,40 +5,49 @@ module.exports = async ({
 }) => {
     const Promise = require('bluebird');
     const cheerio = require('cheerio');
-    const flatten = require('lodash/flatten');
+    const flattenDeep = require('lodash/flattenDeep');
     const iconv = require('iconv-lite');
     const { params, http } = require('app-libs');
+    const list = [
+        'http://bang.dangdang.com/books/newhotsales/01.00.00.00.00.00-24hours-0-0-1-',
+        'http://bang.dangdang.com/books/newhotsales/01.54.00.00.00.00-24hours-0-0-1-',
+    ];
+    const parseUrl = async (url) => {
+        const htmlString = await http.get({
+            uri: url,
+            headers: {
+                'User-Agent': params.ua.pc
+            },
+            encoding: null,
+        });
+
+        const $ = cheerio.load(iconv.decode(htmlString, 'gb2312'));
+        const newBookList = [];
+
+        $('.bang_list > li').each(function () {
+            newBookList.push({
+                title: $(this).find('.name a').attr('title'),
+                url: $(this).find('.name a').attr('href'),
+                cover: $(this).find('.pic img').attr('src').replace('1_l', '1_u')
+            });
+        });
+
+        return newBookList;
+    };
 
     try {
         const results = await Promise.mapSeries(offsets, async (page) => {
             try {
-                const htmlString = await http.get({
-                    uri: `http://bang.dangdang.com/books/newhotsales/01.00.00.00.00.00-24hours-0-0-1-${page}`,
-                    headers: {
-                        'User-Agent': params.ua.pc
-                    },
-                    encoding: null,
+                return await Promise.mapSeries(list, async (url) => {
+                    return await parseUrl(`${url}${page}`);
                 });
-
-                const $ = cheerio.load(iconv.decode(htmlString, 'gb2312'));
-                const newBookList = [];
-
-                $('.bang_list > li').each(function () {
-                    newBookList.push({
-                        title: $(this).find('.name a').attr('title'),
-                        url: $(this).find('.name a').attr('href'),
-                        cover: $(this).find('.pic img').attr('src').replace('1_l', '1_u')
-                    });
-                });
-
-                return newBookList;
             } catch (err) {
                 console.error(err);
                 return [];
             }
         });
 
-        return flatten(results);
+        return flattenDeep(results);
     } catch (err) {
         console.error(err);
         return [];
--- app-modules/updateInitium/index.js
+++ app-modules/updateInitium/index.js
@@ -9,7 +9,7 @@ module.exports = async () => {
     const title = 'Initium有更新~';
     let data = [{
       title: '每日简报',
-      url: `${process.env.proxyUrl}https://theinitium.com`,
+      url: `https://node-aliyun-hk-1.etherdream.com:8443/-----https://theinitium.com/channel/news-brief/`,
     }];
 
     sendMail({
