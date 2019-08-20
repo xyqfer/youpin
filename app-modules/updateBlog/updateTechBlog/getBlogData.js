e5cfb2886999eec72ad1cf6069ce8a0f69514281
凉凉 <xyqfer@gmail.com>
2019-08-20 15:28:47 +0800

    update

--- app-modules/updateBlog/updateTechBlog/getBlogData.js
+++ app-modules/updateBlog/updateTechBlog/getBlogData.js
@@ -36,6 +36,7 @@ module.exports = async () => {
     'https://coolshell.cn/feed',
     'https://www.codesky.me/feed/',
     'https://rsshub.avosapps.us/zhihu/zhuanlan/vczh-nichijou?limit=3',
+    'https://rsshub.avosapps.us/zhihu/people/answers/zi-yun-fei?limit=3',
     'https://rsshub.avosapps.us/custom/http%3A%2F%2Fblog.devtang.com%2Fatom.xml?limit=3',
     'https://rsshub.avosapps.us/custom/https%3A%2F%2Fhijiangtao.github.io%2Ffeed?limit=3',
     'https://rsshub.avosapps.us/github/issue/riskers/blog?limit=3',
