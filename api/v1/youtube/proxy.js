dd637d426779c9342bc785a562dc25c7b94dd320
凉凉 <xyqfer@gmail.com>
2019-09-21 12:10:01 +0800

    update

--- api/v1/youtube/proxy.js
+++ api/v1/youtube/proxy.js
@@ -5,6 +5,7 @@ const ytdl = require('ytdl-core');
 module.exports = async (req, res) => {
   const { id } = req.params;
 
+  res.set('Content-Type', 'video/mp4');
   ytdl(`http://www.youtube.com/watch?v=${id}`)
     .pipe(res);
 };
