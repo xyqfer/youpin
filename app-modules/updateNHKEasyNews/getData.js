d84aba1fd081226730ada04f31df94429822bb24
凉凉 <xyqfer@gmail.com>
2019-09-22 18:49:18 +0800

    update

--- app-modules/updateNHKEasyNews/getData.js
+++ app-modules/updateNHKEasyNews/getData.js
@@ -6,7 +6,7 @@ const {
 } = require('app-libs');
 
 module.exports = async () => {
-    const [data] = await http.get({
+    const result = await http.get({
         uri: 'https://www3.nhk.or.jp/news/easy/news-list.json',
         headers: {
             'User-Agent': params.ua.pc
@@ -14,6 +14,11 @@ module.exports = async () => {
         json: true,
     });
 
+    if (typeof result === 'string') {
+        result = JSON.parse(result.replace(/^\s/g, ''));
+    }
+
+    const data = result[0];
     const newsList = [];
     Object.values(data).forEach((items) => {
         items.forEach((news) => {
@@ -29,7 +34,7 @@ module.exports = async () => {
 
             if (news.has_news_easy_voice) {
                 const date = news.news_prearranged_time.split(' ')[0].replace(/-/g, '');
-                newsData.audioUrl = `https://newswebeasy-assets.gitlab.io/${date}/${news.id}/${news.news_easy_voice_uri}`;
+                newsData.audioUrl = `https://newswebeasy-assets.gitlab.io/${date}/${news.news_id}/${news.news_easy_voice_uri}`;
             }
 
             newsList.push(newsData);
