edad31331258de57d50c42ba1f8427a6a506a92e
凉凉 <xyqfer@gmail.com>
2019-09-23 22:10:58 +0800

    update

--- api/v1/bt/index.js
+++ api/v1/bt/index.js
@@ -5,8 +5,11 @@ const WebTorrent = require('webtorrent')
 module.exports = async (req, res) => {
     const client = new WebTorrent();
     const torrentId = process.env.torrentId;
+    const onResponse = (torrent) => {
+        if (torrent.files.length > 0) {
+            torrent.files[0].createReadStream().pipe(res);
+        }
+    };
 
-    client.add(torrentId, function (torrent) {
-        torrent.files[0].createReadStream().pipe(res);
-    });
+    client.add(torrentId, onResponse);
 };
