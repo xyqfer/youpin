e2780e4f2c992c78c81d524f46f98ae0408e9b50
凉凉 <xyqfer@gmail.com>
2019-09-23 23:26:21 +0800

    update

--- api/v1/bt/index.js
+++ api/v1/bt/index.js
@@ -34,6 +34,9 @@ module.exports = async (req, res) => {
     }
 
     utils.getBt(torrentId, (data) => {
+        if (rangeObj.start && rangeObj.end) {
+            res.set('Content-Range', `bytes ${rangeObj.start}-${rangeObj.end}/${data.length}`);
+        }
         data.file.createReadStream(rangeObj).pipe(res);
     });
 };
