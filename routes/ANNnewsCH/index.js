c1e8f0608d60b9056f0ea642fc676c8e7445bab1
凉凉 <xyqfer@gmail.com>
2019-09-27 16:34:43 +0800

    update

--- routes/ANNnewsCH/index.js
+++ routes/ANNnewsCH/index.js
@@ -3,14 +3,14 @@ const parser = new Parser();
 
 module.exports = async (req, res) => {
     const feed = await parser.parseURL(process.env.ANNnewsCH_rss);
-    const content = feed.items.reduce((acc, item) => {
+    const content = feed.items.slice(0, 5).reduce((acc, item) => {
         acc += `
             <div style="margin-bottom: 30px">
                 <div>${item.title}</div>
                 <div>
                     <video controls webkit-playsinline playsinline src="${item.enclosure.url}"></video>
                 </div>
-                <div style="margin-bottom: 10px">
+                <div style="margin-top: 20px">
                     ${item.content}
                 </div>
             </div>
