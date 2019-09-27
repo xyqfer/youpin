c87c2ffdb060831dcc6b5cbf41a24c8c309d384f
凉凉 <xyqfer@gmail.com>
2019-09-27 16:30:04 +0800

    update

---
+++ routes/ANNnewsCH/index.js
@@ -0,0 +1,26 @@
+const Parser = require('rss-parser');
+const parser = new Parser();
+
+module.exports = async (req, res) => {
+    const feed = await parser.parseURL(process.env.ANNnewsCH_rss);
+    const content = feed.items.reduce((acc, item) => {
+        acc += `
+            <div style="margin-bottom: 30px">
+                <div>${item.title}</div>
+                <div>
+                    <video controls webkit-playsinline playsinline src="${item.enclosure.url}"></video>
+                </div>
+                <div style="margin-bottom: 10px">
+                    ${item.description}
+                </div>
+            </div>
+        `;
+
+        return acc;
+    }, '');
+
+    res.render('archive', {
+        title: 'ANNnewsCH',
+        content,
+    });
+};
