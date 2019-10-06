dab60f13d223f10d7c351b27e1a14563b86e52d4
凉凉 <xyqfer@gmail.com>
2019-10-06 11:02:08 +0800

    remove test code

--- server.js
+++ server.js
@@ -3,21 +3,6 @@
 const { initCloudEngine } = require('@xyqfer/init-leancloud-engine');
 initCloudEngine();
 
-function init() {
-  const AV = require('leanengine');
-
-  console.log(process.env.LEANCLOUD_APP_ID, process.env.LEANCLOUD_APP_KEY, process.env.LEANCLOUD_APP_MASTER_KEY)
-  
-  AV.init({
-    appId: process.env.LEANCLOUD_APP_ID,
-    appKey: process.env.LEANCLOUD_APP_KEY,
-    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
-  });
-  
-  AV.Cloud.useMasterKey();
-}
-// init();
-
 const app = require('./app');
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
