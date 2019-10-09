908e3dcf6949ec4f78121b5896a7082e56d9aeee
凉凉 <xyqfer@gmail.com>
2019-10-09 21:16:42 +0800

    rssbot 接入 cache

--- app-modules/updateRSSBot/index.js
+++ app-modules/updateRSSBot/index.js
@@ -1,82 +1,42 @@
 'use strict';
 
 module.exports = async () => {
-  const rp = require('request-promise');
-  const flattenDeep = require('lodash/flattenDeep');
   const getRSSData = require('./getRSSData');
-  const { getDbData, getDbCount, saveDbData } = require('app-libs/db');
-
-  const dbName = 'RSSDATA';
+  const { db, http, } = require('app-libs');
+  const { cache } = db;
 
   try {
+    const dbName = 'RSSDATA';
     const filterKey = 'link';
-    const fetchAllDbData = async () => {
-      const offsets = [];
-      const per = 1000;
 
-      const dbCount = await getDbCount({
-          dbName,
+    const initCache = () => {
+      await cache.init({
+        dbName,
+        query: {
+          descending: ['createdAt'],
+          select: [filterKey],
+        },
+        limit: 10 * 1000,
       });
-      const maxDbFetchTimes = 10;
-      const limit = Math.min(Math.ceil(dbCount / per), maxDbFetchTimes);
-
-      for (let offset = 0; offset < limit; offset += 1) {
-          offsets.push(offset * per);
-      }
-
-      let dbData = await Promise.mapSeries(offsets, async (offset) => {
-          const dbData = await getDbData({
-              dbName,
-              query: {
-                  descending: ['updatedAt'],
-                  skip: [offset],
-                  select: [filterKey],
-              },
-          });
-
-          return dbData;
-      });
-      return flattenDeep(dbData);
     };
 
-
-    let [dbData, rssData] = await Promise.all([
-      fetchAllDbData(),
+    const [, rssData] = await Promise.all([
+      initCache(),
       getRSSData(),
     ]);
-    dbData = dbData.map(({ link }) => {
-      return link;
-    });
-
-    let newData = rssData.filter(({ link }) => {
-      return !dbData.includes(link);
-    });
-    newData = await Promise.filter(newData, async (item) => {
-      const dbItem = await getDbData({
-        dbName,
-        limit: 1,
-        query: {
-          equalTo: [filterKey, item[filterKey]]
-        }
-      });
-
-      return dbItem.length === 0;
-    }, {
-      concurrency: 1
+    const newData = await cache.findAndSet({
+      dbName,
+      source: rssData,
+      key: filterKey,
     });
 
     if (newData.length > 0) {
-      saveDbData({
-        dbName,
-        data: newData,
-      });
-
       const message = newData.reduce((acc, { title, link }, index) => {
         const isLast = index === newData.length - 1;
         acc += `${title.trim()} ${link}${isLast ? '' : '\n\n'}`;
         return acc;
       }, 'RSSBOT 有更新:\n\n');
-      rp.post({
+      http.post({
         uri: process.env.qqboturl,
         json: true,
         body: {
