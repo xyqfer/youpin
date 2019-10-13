7cdfb221934efdbf2df720ce6fccb1cc66b8b5c2
凉凉 <xyqfer@gmail.com>
2019-10-13 21:02:30 +0800

    rewrite update github

--- app-modules/updateGitHubTrending/index.js
+++ app-modules/updateGitHubTrending/index.js
@@ -1,95 +1,39 @@
 'use strict';
 
-const _ = require('lodash');
+const updateContainer = require('app-containers/update');
 const getGitHubData = require('./getGitHubData');
-const {
-    db: {
-        getDbData,
-        getDbCount,
-        saveDbData
-    },
-    mail: sendMail
-} = require('app-libs');
 
 module.exports = async () => {
     const dbName = 'GitHubTrending';
     const filterKey = 'name';
 
     try {
-        const per = 1000;
-        const dbCount = await getDbCount({
+        return await updateContainer({
             dbName,
-        });
-        const maxDbFetchTimes = 10;
-        const times = Math.min(Math.ceil(dbCount / per), maxDbFetchTimes);
-        const offsets = _.times(times, (i) => {
-            return i * per;
-        });
-
-        let dbData = await Promise.mapSeries(offsets, async (offset) => {
-            const dbData = await getDbData({
-                dbName,
-                query: {
-                    descending: ['updatedAt'],
-                    skip: [offset],
+            filterKey,
+            getTargetData: () => {
+                return getGitHubData();
+            },
+            mail: {
+                title: 'GitHub NEW Trending',
+                template: ({ url = '', name = '', desc = '', lang = '', }) => {
+                    return `
+                        <div style="margin-bottom: 50px">
+                            <a href="${url}" target="_blank">
+                                <h4>${name}</h4>
+                            </a>
+                            <p>
+                                ${desc}
+                            </p>
+                            <p>
+                                ${lang}
+                            </p>
+                        </div>
+                    `;
                 },
-            });
-
-            return dbData;
+                device: 'device2',
+            },
         });
-        dbData = _.flattenDeep(dbData);
-
-        const githubData = await getGitHubData();
-        let newData = _.differenceBy(githubData, dbData, filterKey);
-
-        if (newData.length > 0) {
-            newData = await Promise.filter(newData, async (item) => {
-                const dbItem = await getDbData({
-                    dbName,
-                    limit: 1,
-                    query: {
-                        equalTo: [filterKey, item[filterKey]]
-                    }
-                });
-
-                return dbItem.length === 0;
-            }, {
-                concurrency: 1
-            });
-
-            if (newData.length > 0) {
-                saveDbData({
-                    dbName,
-                    data: newData.map((item) => {
-                        return {
-                            name: item.name
-                        };
-                    })
-                });
-                sendMail({
-                    title: 'GitHub NEW Trending',
-                    data: newData,
-                    template: ({ url = '', name = '', desc = '', lang = '' }) => {
-                        return `
-                            <div style="margin-bottom: 50px">
-                                <a href="${url}" target="_blank">
-                                    <h4>${name}</h4>
-                                </a>
-                                <p>
-                                    ${desc}
-                                </p>
-                                <p>
-                                    ${lang}
-                                </p>
-                            </div>
-                        `;
-                    },
-                    device: 'device2',
-                });
-            }
-        }
-
-        return newData;
     } catch (err) {
         console.error(err);
         return {
