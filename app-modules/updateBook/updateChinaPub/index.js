85e3b92e561dad134c957d26c9e4636a5dfed563
凉凉 <xyqfer@gmail.com>
2019-10-14 20:45:01 +0800

    修复 chinapub 为空时的推送

--- app-modules/updateBook/updateChinaPub/index.js
+++ app-modules/updateBook/updateChinaPub/index.js
@@ -16,26 +16,28 @@ module.exports = async () => {
             getTargetData: () => getChinaPubData(),
             notify: async function(data) {
                 const newData = await getNewData(data);
-                return sendMail({
-                    title: 'ChinaPub 有新书啦',
-                    data: newData,
-                    template: ({ url = '', name = '', intro = '', cover = '' }) => {
-                        const bookUrl = `${process.env.hostName}/api/v1/book/redirect?url=${encodeURIComponent(url)}`;
-                        return `
-                            <div style="margin-bottom: 30px">
-                                <a href="${bookUrl}" target="_blank">
-                                    <h4>${name}</h4>
-                                </a>
-                                <p>
-                                    ${intro}
-                                </p>
-                                <div>
-                                    <img src="${cover}" alt="">
+                if (newData.length > 0) {
+                    return sendMail({
+                        title: 'ChinaPub 有新书啦',
+                        data: newData,
+                        template: ({ url = '', name = '', intro = '', cover = '' }) => {
+                            const bookUrl = `${process.env.hostName}/api/v1/book/redirect?url=${encodeURIComponent(url)}`;
+                            return `
+                                <div style="margin-bottom: 30px">
+                                    <a href="${bookUrl}" target="_blank">
+                                        <h4>${name}</h4>
+                                    </a>
+                                    <p>
+                                        ${intro}
+                                    </p>
+                                    <div>
+                                        <img src="${cover}" alt="">
+                                    </div>
                                 </div>
-                            </div>
-                        `;
-                    },
-                });
+                            `;
+                        },
+                    });
+                }
             },
         });
     } catch (err) {
