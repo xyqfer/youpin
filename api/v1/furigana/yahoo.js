d78c1c0bffa6d988a1488f2a25a4026531f02638
凉凉 <xyqfer@gmail.com>
2019-10-22 09:39:18 +0800

    update furigana

--- api/v1/furigana/yahoo.js
+++ api/v1/furigana/yahoo.js
@@ -2,23 +2,14 @@ const { http } = require('app-libs');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const jisx0208 = fs.readFileSync(`${__dirname}/jis-x-0208-utf8.txt`);
-
-module.exports = async (content = '') => {
-    let jisxStr = '';
-    for (let i = 0; i < content.length; i++) {
-        const str = content[i];
-        if (str === '|' || jisx0208.includes(str)) {
-            jisxStr += str;
-        }
-    }
-
+const fetchFurigana = async (sentence) => {
     const data = await http.post({
         uri: 'https://jlp.yahooapis.jp/FuriganaService/V1/furigana',
         headers: {
             'User-Agent': `Yahoo AppID:  ${process.env.YAHOO_APP_ID}`,
         },
         form: {
-            sentence: jisxStr,
+            sentence,
             grade: 1,
         },
     });
@@ -48,5 +39,24 @@ module.exports = async (content = '') => {
         }
     });
 
+    return result;
+};
+
+module.exports = async (content = '') => {
+    let result = [];
+
+    try {
+        result = await fetchFurigana(content);
+    } catch(err) {
+        let jisxStr = '';
+        for (let i = 0; i < content.length; i++) {
+            const str = content[i];
+            if (str === '|' || jisx0208.includes(str)) {
+                jisxStr += str;
+            }
+        }
+        result = await fetchFurigana(jisxStr);
+    }
+    
     return result;
 };
