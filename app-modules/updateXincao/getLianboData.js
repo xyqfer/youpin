54f6344338a8d074dcb2578c7bd4264da97e0582
凉凉 <xyqfer@gmail.com>
2019-12-08 10:24:04 +0800

    update lianbo

---
+++ app-modules/updateXincao/getLianboData.js
@@ -0,0 +1,15 @@
+'use strict';
+const { http } = require('app-libs');
+
+module.exports = async () => {
+    const result = await http.get({
+        uri: 'https://api.cntv.cn/list/getWxArticleList?id=PAGEb3A73LquTUFIbR5GjLgg180411&serviceId=lianboplus&date=',
+        json: true,
+    });
+    const data = result.videoList.map((item) => ({
+        title: item.article_title,
+        url: item.article_url,
+    }));
+
+    return data[0];
+};
