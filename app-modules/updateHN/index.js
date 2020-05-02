0330d8728706475902e0d632a7efbc05d08d7139
xyqfer <xyqfer@gmail.com>
2020-05-02 16:49:14 +0800

    update style

--- app-modules/updateHN/index.js
+++ app-modules/updateHN/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const updateContainer = require('app-containers/update');
 const fetchRSS = require('app-containers/fetchRSS');
+const cheerio = require('cheerio');
 
 module.exports = async () => {
     const dbName = 'RSSData2';
@@ -29,6 +30,17 @@ module.exports = async () => {
                 fetchRSS({
                     source: 'RSS_HN',
                     field: ['title', 'link', 'content'],
+                    map: (item) => {
+                        const { title, link, content } = item;
+                        const $ = cheerio.load(content);
+                        $('img').removeAttr('style');
+
+                        return {
+                            title,
+                            link,
+                            summary: $.html(),
+                        };
+                    },
                 }),
         });
     } catch (err) {
