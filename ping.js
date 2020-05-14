13ee1cc4263916e6883ab2749b1a9e533ea3bb09
xyqfer <xyqfer@gmail.com>
2020-05-14 15:16:20 +0800

    rollback

--- ping.js
+++ ping.js
@@ -2,7 +2,6 @@ const https = require('https');
 
 const ping = () => {
     https.get(`https://${process.env.LEANCLOUD_APP_DOMAIN}.avosapps.us/`, () => {}).on('error', () => {});
-    https.get('https://xyx74gq3d1.run-us-west2.goorm.io/', () => {}).on('error', () => {});
     
     const date = new Date();
     const hour = date.getHours();
