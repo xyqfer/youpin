715594ee356295c05294da1d39fb821c501421fd
xyqfer <xyqfer@gmail.com>
2020-08-06 15:43:58 +0800

    fix todo add

--- api/v1/todo/add.js
+++ api/v1/todo/add.js
@@ -1,20 +1,21 @@
-const { saveDbData } = require('app-libs/db');
+const AV = require('leanengine');
 
 module.exports = async (req, res) => {
     const { text } = req.body;
     const now = Date.now();
 
-    await saveDbData({
-        dbName: 'TODO',
-        data: [
-            {
-                text,
-                createTime: now,
-                updateTime: now
-            },
-        ],
-    });
+    const Todo = AV.Object.extend('TODO');
+    const todo = new Todo();
+
+    todo.set('text', text);
+    todo.set('createTime', now);
+    todo.set('updateTime', now);
+
+    const data = await todo.save();
     res.json({
         success: true,
+        data: {
+            todo: data
+        }
     });
 };
