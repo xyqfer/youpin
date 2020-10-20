b6be0b32a3f1cb09ed289fdcfdccd1436da18a77
xyqfer <xyqfer@gmail.com>
2020-10-20 16:55:45 +0800

    update notify

--- app-modules/rechargeHK/index.js
+++ app-modules/rechargeHK/index.js
@@ -4,10 +4,13 @@ const { mail: sendMail } = require('app-libs');
 
 module.exports = () => {
     sendMail({
-        title: 'HK 手机卡充值提醒',
+        title: 'HK 手机卡充值提醒 & 八达通使用提醒',
         data: [{
             url: 'https://coderschool.cn/3331.html',
-            title: '教程',
+            title: '手机卡',
+        }, {
+            url: 'https://51.ruyo.net/16422.html',
+            title: '八达通',
         }],
         template: ({ url = '', title = '' }) => `
             <div style="margin-bottom: 50px">
