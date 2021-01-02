546beee109a994806b3b16840943152cb96df7e4
xyqfer <xyqfer@gmail.com>
2021-01-02 22:52:04 +0800

    fix id

--- api/v1/caixin/redirect.js
+++ api/v1/caixin/redirect.js
@@ -1,16 +1,14 @@
-const cheerio = require('cheerio');
+const path = require('path');
 const { http } = require('app-libs');
 
 module.exports = async (req, res) => {
     const { url } = req.query;
+    const u = new URL(url);
+    const id = path.basename(u.pathname, '.html');
 
     const response = await http.get({
-        uri: url,
+        uri: `https://gateway.caixin.com/api/purchase/article/get/${id}`,
     });
-
-    const $ = cheerio.load(response);
-    const link = $('.openInApp').attr('href');
-    const id = (new URL(link)).searchParams.get('id');
     
-    res.redirct(`api/v1/caixin/article?id=${id}`);
+    res.redirct(`/api/v1/caixin/article?id=${response.data.data.id}`);
 };
