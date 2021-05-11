184b704782be9616f5307446a54d77f373ff71a3
xyqfer <xyqfer@gmail.com>
2021-05-11 23:23:40 +0800

    update

--- api/v1/readability/view.js
+++ api/v1/readability/view.js
@@ -1,8 +1,12 @@
-const { readability } = require('app-libs');
+const { readability, http } = require('app-libs');
 
 module.exports = async (req, res) => {
     const { url } = req.query;
-    const { title, content } = await readability(url);
+    // const { title, content } = await readability(url);
+    const { title, content } = await http.get({
+      uri: process.env.READER_VIEW_URL2 + encodeURIComponent(url),
+      json: true,
+    });
 
     res.render('archive', {
         title,
