ed5f5973bbb8c471571cc53d6afdcd31b63912d9
xyqfer <xyqfer@gmail.com>
2021-05-15 22:37:26 +0800

    fix timeout

--- api/v1/readability/view.js
+++ api/v1/readability/view.js
@@ -2,11 +2,11 @@ const { readability, http } = require('app-libs');
 
 module.exports = async (req, res) => {
     const { url } = req.query;
-    // const { title, content } = await readability(url);
-    const { title, content } = await http.get({
-      uri: process.env.READER_VIEW_URL2 + encodeURIComponent(url),
-      json: true,
-    });
+    const { title, content } = await readability(url);
+    // const { title, content } = await http.get({
+    //   uri: process.env.READER_VIEW_URL2 + encodeURIComponent(url),
+    //   json: true,
+    // });
 
     res.render('archive', {
         title,
