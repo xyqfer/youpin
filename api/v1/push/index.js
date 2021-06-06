9f977772432b44c9874496053e35524b53fc8a71
xyqfer <xyqfer@gmail.com>
2021-06-06 22:53:46 +0800

    update push

--- api/v1/push/index.js
+++ api/v1/push/index.js
@@ -1,4 +1,6 @@
+const { v4: uuidv4 } = require('uuid');
 const { mail: sendMail } = require('app-libs');
+const { saveDbData } = require('app-libs/db')
 
 module.exports = async (req, res) => {
   const { token, title = '' } = req.query;
@@ -10,16 +12,31 @@ module.exports = async (req, res) => {
   }
 
   try {
+    const uuid = uuidv4();
+    const template = ({ url = '', title = '' }) => `
+      <div style="margin-bottom: 50px">
+          <a href="${url}" target="_blank">
+              <h4>${title}</h4>
+          </a>
+      </div>
+    `;
+    const content = newData.map(template).join('');
+
+    await saveDbData({
+        dbName: 'Archive',
+        data: [
+            {
+                uuid,
+                title,
+                content,
+            },
+        ],
+    });
+
     sendMail({
       title,
       data: newData,
-      template: ({ url = '', title = '' }) => `
-        <div style="margin-bottom: 50px">
-            <a href="${url}" target="_blank">
-                <h4>${title}</h4>
-            </a>
-        </div>
-      `,
+      template,
     });
   } catch(err) {
     console.error('push err');
