0a38eae6a7b5804f9ef8f26285dd8a0dead2771e
凉凉 <xyqfer@gmail.com>
2021-09-06 15:59:40 +0800

    Update redirect.js

--- api/v1/caixin/redirect.js
+++ api/v1/caixin/redirect.js
@@ -5,11 +5,6 @@ module.exports = async (req, res) => {
     const { url } = req.query;
     const u = new URL(url);
     const id = path.basename(u.pathname, '.html');
-
-    const response = await http.get({
-        json: true,
-        uri: `https://gateway.caixin.com/api/purchase/article/get/${id}`,
-    });
     
-    res.redirect(`/api/v1/caixin/article?id=${response.data.id}`);
+    res.redirect(`/api/v1/caixin/article?id=${id}`);
 };
