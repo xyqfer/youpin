e56755e6ff19090021dfc7508e83d7ace1eb9c40
凉凉 <xyqfer@gmail.com>
2021-10-26 12:09:03 +0800

    update apple watch

--- app-modules/updateAppleWatch/index.js
+++ app-modules/updateAppleWatch/index.js
@@ -7,13 +7,15 @@ module.exports = async () => {
             uri: 'https://www.apple.com.cn/shop/fulfillment-messages?parts.0=MKMX3CH%2FA&searchNearby=true&mt=regular&option.0=&store=R639&_=1635176710530',
             json: true,
         });
-        const info = data.body.content.pickupMessage.stores[1].partsAvailability['MKMX3CH/A'].pickupSearchQuote
+        const info0 = data.body.content.pickupMessage.stores[0].partsAvailability['MKMX3CH/A'].pickupSearchQuote
+        const info1 = data.body.content.pickupMessage.stores[1].partsAvailability['MKMX3CH/A'].pickupSearchQuote
 
-        try {
-            // const res = await lark.sendText(process.env.LARK_USER, 'Apple Watch 有货啦！！！');
-            const res = await lark.sendText(process.env.LARK_USER, info);
-        } catch(err) {
-            console.error(err);
+        if (info0 !== '不可取货' || info1 !== '不可取货') {
+            try {
+                const res = await lark.sendText(process.env.LARK_USER, 'Apple Watch 有货啦！！！');
+            } catch(err) {
+                console.error(err);
+            }
         }
 
         return {
