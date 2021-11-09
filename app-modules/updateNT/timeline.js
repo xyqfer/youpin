e09976e11ae31f669a15423fbbca0bf3affc8405
凉凉 <xyqfer@gmail.com>
2021-11-09 15:31:28 +0800

    add slice and sleep

--- app-modules/updateNT/timeline.js
+++ app-modules/updateNT/timeline.js
@@ -1,11 +1,24 @@
 const update = require('./update');
 const { getFriends } = require('./utils');
 
+const sleep = (time) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, time * 1000)
+    })
+}
+
 module.exports = async (user = '') => {
     try {
         const userList = await getFriends(user)
 
-        return await update(userList, true);
+        for (let i = 0; i < userList.length; i += 100) {
+            await update(userList.slice(i, i + 100), true);
+            await sleep(10);
+        }
+
+        return {
+            success: true
+        }
     } catch (err) {
         console.error(err);
         return {
