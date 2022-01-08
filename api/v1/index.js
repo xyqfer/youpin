3f452128c0dc19518d0b50e2c8e82de5ab4bdc53
凉凉 <xyqfer@gmail.com>
2022-01-08 21:58:48 +0800

    fix header

--- api/v1/index.js
+++ api/v1/index.js
@@ -83,7 +83,9 @@ router.post('/alipayscheme/update', require('./alipayscheme/update'));
 
 router.get('/proxyimage', async (req, res) => {
     const { url } = req.query;
-    const headers = {};
+    const headers = {
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+    };
 
     if (req.headers.range) {
         headers.Range = req.headers.range;
